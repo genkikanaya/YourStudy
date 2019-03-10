6541377648f5262f89ef75a4d265ab78d53233e9
kanayagenki <shironeko1123@gmail.com>
2019-03-10 16:57:54 +0900

    add-listview-adapter

--- app/src/main/java/com/example/kanayagenki/yourstudy/MainActivity.kt
+++ app/src/main/java/com/example/kanayagenki/yourstudy/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.kanayagenki.yourstudy
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val subjects = arrayOf("さんすう", "えいご", "ぎりしあもじ")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, subjects)
+        subjectView.adapter = adapter
     }
 }
